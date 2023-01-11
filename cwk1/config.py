bc2e5e5c9357ed8dfaf5b7a58a6f729dabe04754
JoshuaGraham14 <59918630+JoshuaGraham14@users.noreply.github.com>
2023-01-11 15:10:01 +0000

    Delete cwk1 directory

--- cwk1/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-WTF_CSRF_ENABLED = True
-
-SECRET_KEY = 'joshuagraham'
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_TRACK_MODIFICATIONS = True
